1c0c29578d5330b96c1742434422b3bbc937d93b
Shuliak Leonid <l.shulyak@tanto-s.ru>
2021-07-07 20:56:36 +0300

    add grammar-test page with routing

--- exadel-trainee-app/src/app/app.module.ts
+++ exadel-trainee-app/src/app/app.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginModule } from './models/login/login.module';
+import { GrammarTestModule } from './models/grammar-test/grammar-test.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GrammarTestComponent } from './models/grammar-test/grammar-test.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, LoginModule,
-    GrammarTestComponent, AppRoutingModule, BrowserAnimationsModule],
+    GrammarTestModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- exadel-trainee-app/src/app/models/grammar-test/grammar-test.module.ts
+++ exadel-trainee-app/src/app/models/grammar-test/grammar-test.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GrammarTestRoutingModule } from './grammar-test-routing.module';
+import {GrammarTestComponent} from './grammar-test.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GrammarTestComponent],
   imports: [
     CommonModule,
-    GrammarTestRoutingModule
+    GrammarTestRoutingModule,
   ]
 })
 export class GrammarTestModule { }
