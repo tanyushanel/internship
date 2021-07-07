6913fa1e446be6560364a4d5c7a40250fd8647e5
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2021-07-07 19:54:41 +0300

    refactor: rename files in login module

--- exadel-trainee-app/src/app/models/login/login.module.ts
+++ exadel-trainee-app/src/app/models/login/login.module.ts
@@ -4,11 +4,11 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from '../../app.component';
 import { LoginComponent } from './login.component';
-import { LoginRoutingModel } from './login-routing.model';
+import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, BrowserModule, BrowserAnimationsModule, LoginRoutingModel],
+  imports: [CommonModule, BrowserModule, BrowserAnimationsModule, LoginRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
