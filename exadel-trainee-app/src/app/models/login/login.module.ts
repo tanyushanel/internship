5b7809aa3f8202de9e1d445c3cd01528b9bb42ae
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2021-07-08 23:15:59 +0300

    feat: add submit btn

--- exadel-trainee-app/src/app/models/login/login.module.ts
+++ exadel-trainee-app/src/app/models/login/login.module.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
@@ -18,6 +19,7 @@ import { LoginRoutingModule } from './login-routing.module';
     MatInputModule,
     ReactiveFormsModule,
     FormsModule,
+    MatButtonModule,
   ],
   exports: [],
 })
