34397ca4655b5d2a404e7a127340c6a69a6e6951
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2021-07-08 21:11:11 +0300

    fix: fix login module file. add router-link on app view

--- exadel-trainee-app/src/app/models/login/login.module.ts
+++ exadel-trainee-app/src/app/models/login/login.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from '../../app.component';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, BrowserModule, BrowserAnimationsModule, LoginRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  imports: [CommonModule, LoginRoutingModule],
 })
 export class LoginModule {}
