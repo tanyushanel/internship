398b99286bd160b63efc7a1eb6df13563a59c281
Shuliak Leonid <l.shulyak@tanto-s.ru>
2021-07-08 11:08:26 +0300

    fix lazy loading, delete import modules

--- exadel-trainee-app/src/app/app.module.ts
+++ exadel-trainee-app/src/app/app.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginModule } from './models/login/login.module';
-import { GrammarTestModule } from './models/grammar-test/grammar-test.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    LoginModule,
-    GrammarTestModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
