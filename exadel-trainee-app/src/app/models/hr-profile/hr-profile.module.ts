867ce57b21e880255eb05f1b6fc6b1b11bb52b4e
Andrei <casterweb@tut.by>
2021-07-09 16:14:08 +0300

    rename hr-profile-routing module

--- exadel-trainee-app/src/app/models/hr-profile/hr-profile.module.ts
+++ exadel-trainee-app/src/app/models/hr-profile/hr-profile.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HrProfileComponent } from './hr-profile.component';
-import { HrProfileRoutingModule } from './hr-profile.-routing.module';
+import { HrProfileRoutingModule } from './hr-profile-routing.module';
 
 @NgModule({
   declarations: [HrProfileComponent],
