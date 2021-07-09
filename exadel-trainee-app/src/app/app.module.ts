873a0b545e7869229abf2300b0759d322da27c3a
tanyushanel <tanyushanel@users.noreply.github.com>
2021-07-09 19:38:28 +0200

    fix PR

--- exadel-trainee-app/src/app/app.module.ts
+++ exadel-trainee-app/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { SidebarModule } from './components/sidebar/sidebar.module';
-import { UserProfileComponent } from './models/user-profile/user-profile.component';
 
 @NgModule({
-  declarations: [AppComponent, UserProfileComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SidebarModule],
   providers: [],
   bootstrap: [AppComponent],
--- exadel-trainee-app/src/app/models/user-profile/user-profile.module.ts
+++ exadel-trainee-app/src/app/models/user-profile/user-profile.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileRoutingModule } from './user-profile-routing.module';
+import { UserProfileComponent } from './user-profile.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [UserProfileComponent],
   imports: [CommonModule, UserProfileRoutingModule],
 })
 export class UserProfileModule {}
