a18a4c78c2f1e4e03bcf0117d3f75241dd6ff934
dimahalaiskiy <laptop@Laptops-MacBook-Pro.local>
2021-07-11 02:15:41 +0300

    change-the-app-module-modifited-to-lazy-load

--- exadel-trainee-app/src/app/app.module.ts
+++ exadel-trainee-app/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginModule } from './models/login/login.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarModule } from './components/sidebar/sidebar.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LoginModule, BrowserAnimationsModule, SidebarModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SidebarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
