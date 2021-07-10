fa16fce318a739d7777e7884e25326576e960dbf
Laptop <laptop@Laptops-MacBook-Pro.local>
2021-07-10 13:37:43 +0300

    add-commonmodule

--- exadel-trainee-app/src/app/models/speaking-test/speaking.module.ts
+++ exadel-trainee-app/src/app/models/speaking-test/speaking.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from '../../app.component';
 import { SpeakingComponent } from './speaking.component';
 import { SpeakingRoutingModule } from './speaking-routing.module';
 
 @NgModule({
   declarations: [SpeakingComponent],
-  imports: [CommonModule, BrowserModule, BrowserAnimationsModule, SpeakingRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  imports: [SpeakingRoutingModule, CommonModule],
 })
 export class SpeakingModule {}
