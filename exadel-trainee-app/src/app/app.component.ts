ea9045b046e68e518b988dee5e8fb6f75e36aeaa
tanyushanel <tanyushanel@users.noreply.github.com>
2021-07-11 18:34:27 +0200

    feat: button & openfunc added

--- exadel-trainee-app/src/app/app.component.ts
+++ exadel-trainee-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { WarningDialogComponent } from './warning-dialog/warning-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exadel-trainee-app';
+
+  constructor(public dialog: MatDialog) {}
+
+  openWarningDialog(): void {
+    this.dialog.open(WarningDialogComponent, {
+      width: '35rem',
+    });
+  }
 }
