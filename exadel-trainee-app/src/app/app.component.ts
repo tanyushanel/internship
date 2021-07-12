f4cee85babc5d012d0e9e7265d4bce3a0fa4803c
tanyushanel <tanyushanel@users.noreply.github.com>
2021-07-12 13:04:16 +0200

    fix: removed unused imports

--- exadel-trainee-app/src/app/app.component.ts
+++ exadel-trainee-app/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { UserResultsDialogComponent } from './models/dialog-module/user-results-dialog/user-results-dialog.component';
 
 @Component({
   selector: 'app-root',
