1ccc62979591dacea51f39507c4cb8f11dca1887
tanyushanel <tanyushanel@users.noreply.github.com>
2021-07-12 15:47:55 +0200

    feat: custom dialog added

--- exadel-trainee-app/src/app/models/dialog-module/warning-dialog/warning-dialog.component.ts
+++ exadel-trainee-app/src/app/models/dialog-module/warning-dialog/warning-dialog.component.ts
@@ -1,3 +1,4 @@
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,5 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./warning-dialog.component.scss'],
 })
 export class WarningDialogComponent implements OnInit {
+  constructor(private dialog: MatDialog) {}
+
   ngOnInit(): void {}
 }
