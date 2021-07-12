7dd93137d09376412b09b52835009ca159d3054a
tanyushanel <tanyushanel@users.noreply.github.com>
2021-07-12 21:43:15 +0200

    feat: html layout radio btns

--- exadel-trainee-app/src/app/models/user-profile/user-profile.module.ts
+++ exadel-trainee-app/src/app/models/user-profile/user-profile.module.ts
@@ -1,10 +1,18 @@
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatRadioModule, MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { UserProfileComponent } from './user-profile.component';
 
 @NgModule({
   declarations: [UserProfileComponent],
-  imports: [CommonModule, UserProfileRoutingModule],
+  imports: [CommonModule, UserProfileRoutingModule, MatRadioModule, MatButtonModule],
+  providers: [
+    {
+      provide: MAT_RADIO_DEFAULT_OPTIONS,
+      useValue: { color: 'accent' },
+    },
+  ],
 })
 export class UserProfileModule {}
