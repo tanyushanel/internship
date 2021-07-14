f4dbe7609fcb535830492ba5be9ebea169105f1a
Serdar <serdaratamuradov24@gmail.com>
2021-07-14 16:31:03 +0300

    created stepper

--- exadel-trainee-app/src/app/app.module.ts
+++ exadel-trainee-app/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CheckComponent } from './components/mock-component/check/check.componen
 import { EditorComponent } from './components/mock-component/editor/editor.component';
 import { ManageComponent } from './components/mock-component/manage/manage.component';
 import { StatisticsComponent } from './components/mock-component/statistics/statistics.component';
+import { StepperComponent } from './components/stepper/stepper.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { StatisticsComponent } from './components/mock-component/statistics/stat
     EditorComponent,
     ManageComponent,
     StatisticsComponent,
+    StepperComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SidebarModule],
   providers: [],
---
+++ exadel-trainee-app/src/app/components/stepper/stepper.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-stepper',
+  templateUrl: './stepper.component.html',
+  styleUrls: ['./stepper.component.scss'],
+})
+export class StepperComponent implements OnInit {
+  firstFormGroup: FormGroup;
+
+  secondFormGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.firstFormGroup = this.formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this.formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+}
