dc2d8377fc8f3505fbd4ae49b9e90ab58d44d09c
tanyushanel <tanyushanel@users.noreply.github.com>
2021-07-14 18:56:03 +0200

    feat: bind table to user-profile

--- exadel-trainee-app/src/app/models/user-profile/user-profile.component.ts
+++ exadel-trainee-app/src/app/models/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Level } from 'src/constants/data-constants';
+import { Test } from '../../interfaces/test';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,6 +8,101 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
+  results: Test[] = [
+    {
+      id: 1,
+      date: { creationDate: new Date().toDateString() },
+      level: Level.advanced,
+      content: {
+        grammar: {
+          id: 1,
+          mark: 10,
+        },
+        audition: {
+          id: 1,
+          mark: 20,
+        },
+        essay: {
+          id: 1,
+          mark: 20,
+        },
+        speaking: {
+          id: 1,
+          mark: 10,
+        },
+      },
+    },
+    {
+      id: 2,
+      date: { creationDate: new Date().toDateString() },
+      level: Level.beginner,
+      content: {
+        grammar: {
+          id: 1,
+          mark: 30,
+        },
+        audition: {
+          id: 1,
+          mark: 20,
+        },
+        essay: {
+          id: 1,
+          mark: 70,
+        },
+        speaking: {
+          id: 1,
+          mark: 0,
+        },
+      },
+    },
+    {
+      id: 3,
+      date: { creationDate: new Date().toDateString() },
+      level: Level.beginner,
+      content: {
+        grammar: {
+          id: 1,
+          mark: 30,
+        },
+        audition: {
+          id: 1,
+          mark: 20,
+        },
+        essay: {
+          id: 1,
+          mark: 10,
+        },
+        speaking: {
+          id: 1,
+          mark: 10,
+        },
+      },
+    },
+    {
+      id: 4,
+      date: { creationDate: new Date().toDateString() },
+      level: Level.beginner,
+      content: {
+        grammar: {
+          id: 1,
+          mark: 30,
+        },
+        audition: {
+          id: 1,
+          mark: 20,
+        },
+        essay: {
+          id: 1,
+          mark: 20,
+        },
+        speaking: {
+          id: 1,
+          mark: 10,
+        },
+      },
+    },
+  ];
+
   ngOnInit() {}
 
   onStartButtonClick(): void {}
--- exadel-trainee-app/src/app/models/user-profile/user-results-table/user-results-table.component.ts
+++ exadel-trainee-app/src/app/models/user-profile/user-results-table/user-results-table.component.ts
@@ -1,8 +1,7 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table/table-data-source';
+import { MatTableDataSource } from '@angular/material/table';
 import { Test } from 'src/app/interfaces/test';
-import { Level } from 'src/constants/data-constants';
 
 @Component({
   selector: 'app-user-results-table',
@@ -10,100 +9,7 @@ import { Level } from 'src/constants/data-constants';
   styleUrls: ['./user-results-table.component.scss'],
 })
 export class UserResultsTableComponent implements AfterViewInit {
-  results: Test[] = [
-    {
-      id: 1,
-      date: { creationDate: new Date().toDateString() },
-      level: Level.advanced,
-      content: {
-        grammar: {
-          id: 1,
-          mark: 10,
-        },
-        audition: {
-          id: 1,
-          mark: 20,
-        },
-        essay: {
-          id: 1,
-          mark: 20,
-        },
-        speaking: {
-          id: 1,
-          mark: 10,
-        },
-      },
-    },
-    {
-      id: 2,
-      date: { creationDate: new Date().toDateString() },
-      level: Level.beginner,
-      content: {
-        grammar: {
-          id: 1,
-          mark: 30,
-        },
-        audition: {
-          id: 1,
-          mark: 20,
-        },
-        essay: {
-          id: 1,
-          mark: 70,
-        },
-        speaking: {
-          id: 1,
-          mark: 0,
-        },
-      },
-    },
-    {
-      id: 3,
-      date: { creationDate: new Date().toDateString() },
-      level: Level.beginner,
-      content: {
-        grammar: {
-          id: 1,
-          mark: 30,
-        },
-        audition: {
-          id: 1,
-          mark: 20,
-        },
-        essay: {
-          id: 1,
-          mark: 10,
-        },
-        speaking: {
-          id: 1,
-          mark: 10,
-        },
-      },
-    },
-    {
-      id: 4,
-      date: { creationDate: new Date().toDateString() },
-      level: Level.beginner,
-      content: {
-        grammar: {
-          id: 1,
-          mark: 30,
-        },
-        audition: {
-          id: 1,
-          mark: 20,
-        },
-        essay: {
-          id: 1,
-          mark: 20,
-        },
-        speaking: {
-          id: 1,
-          mark: 10,
-        },
-      },
-    },
-  ];
+  @Input() results: Test[] = [];
 
   dataSource = new MatTableDataSource(this.results);
 
