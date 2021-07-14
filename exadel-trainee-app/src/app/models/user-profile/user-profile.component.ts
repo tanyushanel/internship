7a61782ebdffa5f3a09303a17f705f3d4f293c02
tanyushanel <tanyushanel@users.noreply.github.com>
2021-07-14 14:49:19 +0200

    feat: caption added

--- exadel-trainee-app/src/app/models/user-profile/user-profile.component.ts
+++ exadel-trainee-app/src/app/models/user-profile/user-profile.component.ts
@@ -4,39 +4,39 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Level, Test } from './mocks';
 
-const results: Test[] = [
-  {
-    id: 1,
-    date: { creationDate: new Date().toDateString() },
-    level: Level.beginner,
-    content: {
-      grammar: {
-        id: 1,
-        mark: 30,
-      },
-      audition: {
-        id: 1,
-        mark: 20,
-      },
-      essay: {
-        id: 1,
-        mark: 20,
-      },
-      speaking: {
-        id: 1,
-        mark: 10,
-      },
-    },
-  },
-];
-
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit, AfterViewInit {
-  dataSource = new MatTableDataSource(results);
+  results: Test[] = [
+    {
+      id: 1,
+      date: { creationDate: new Date().toDateString() },
+      level: Level.beginner,
+      content: {
+        grammar: {
+          id: 1,
+          mark: 30,
+        },
+        audition: {
+          id: 1,
+          mark: 20,
+        },
+        essay: {
+          id: 1,
+          mark: 20,
+        },
+        speaking: {
+          id: 1,
+          mark: 10,
+        },
+      },
+    },
+  ];
+
+  dataSource = new MatTableDataSource(this.results);
 
   displayedColumns: any[] = ['id', 'date', 'level', 'content'];
 
