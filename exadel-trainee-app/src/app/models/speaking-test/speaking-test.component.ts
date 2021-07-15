512802f9053f63c675d1cbcb505e29036985abd0
dimahalaiskiy <dgalaiskiy@icloud.com>
2021-07-15 17:00:04 +0300

    fix-bug

--- exadel-trainee-app/src/app/models/speaking-test/speaking-test.component.ts
+++ exadel-trainee-app/src/app/models/speaking-test/speaking-test.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+declare let MediaRecorder: any;
 
 @Component({
   selector: 'app-speaking',
@@ -6,5 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./speaking-test.component.scss'],
 })
 export class SpeakingTestComponent implements OnInit {
-  ngOnInit(): void {}
+  mediaRecorder: any;
+
+  chunks:Blob[] = [];
+  audioFiles:{src: SafeUrl}[] = [];
+
+  constructor(private cd: ChangeDetectorRef, private dom: DomSanitizer) {
+
+  }
+
+  async ngOnInit() {
+    let stream = null;
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      console.log(stream);
+      this.mediaRecorder = new MediaRecorder(stream);
+      this.mediaRecorder.onstop = () => {
+        console.log('data available after MediaRecorder.stop() called.');
+        const blob = new Blob(this.chunks, { type: 'audio/ogg; codecs=opus' });
+        this.chunks = [];
+        const audioURL = URL.createObjectURL(blob);
+        console.log(this.chunks, blob, audioURL);
+        var audio = {
+          src: this.dom.bypassSecurityTrustUrl(audioURL),
+        };
+        //audio.controls = true;
+        console.log(audio, audioURL, this.dom.bypassSecurityTrustUrl(audioURL));
+        //audio.src = audioURL;
+        this.audioFiles.push(audio);
+        console.log(audioURL);
+        console.log('recorder stopped');
+        this.cd.detectChanges();
+      };
+      this.mediaRecorder.ondataavailable = (e:{data:Blob}) => {
+        this.chunks.push(e.data);
+      };
+  } catch(err) {
+    alert('Error capturing audio.');
+  } finally {
+
+  }
+}
+
+  startRecording() {
+    this.mediaRecorder.start();
+    console.log(this.mediaRecorder.state);
+    console.log('recorder started');
+  }
+  stopRecording() {
+    this.mediaRecorder.stop();
+    console.log(this.mediaRecorder.state);
+    console.log('record stopped');
+  }
 }
--- exadel-trainee-app/src/app/models/speaking-test/speaking-test.module.ts
+++ exadel-trainee-app/src/app/models/speaking-test/speaking-test.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SpeakingTestComponent } from './speaking-test.component';
 import { SpeakingTestRoutingModule } from './speaking-test-routing.module';
 
 @NgModule({
+  imports: [SpeakingTestRoutingModule, CommonModule, FormsModule],
   declarations: [SpeakingTestComponent],
-  imports: [SpeakingTestRoutingModule, CommonModule],
 })
 export class SpeakingTestModule {}
