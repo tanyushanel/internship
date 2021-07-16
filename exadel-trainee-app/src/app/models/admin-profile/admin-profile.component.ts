5806127baa33086e89a4539e1b753cc8b507c6e5
1810198nm@gmail.com <Fiqxu8-qyqtap-wakjuk>
2021-07-16 20:31:04 +0500

    added feature virtual scroll

--- exadel-trainee-app/src/app/models/admin-profile/admin-profile.component.ts
+++ exadel-trainee-app/src/app/models/admin-profile/admin-profile.component.ts
@@ -1,10 +1,286 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
+export interface PeriodicElement {
+  position: number;
+  date: number;
+  level: string;
+  coach: string;
+  expl: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    position: 1,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 2,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 3,
+    level: 'inter',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 4,
+    level: 'ber=g',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 5,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 6,
+    level: 'inter',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 7,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 8,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 9,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 10,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 11,
+    level: 'inter',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 12,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 1,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 2,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 3,
+    level: 'inter',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 4,
+    level: 'ber=g',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 5,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 6,
+    level: 'inter',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 7,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 8,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 9,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 10,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 11,
+    level: 'inter',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 12,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 1,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 2,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 3,
+    level: 'inter',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 4,
+    level: 'ber=g',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 5,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 6,
+    level: 'inter',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 7,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 8,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 9,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 10,
+    level: 'adv',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 11,
+    level: 'inter',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+  {
+    position: 12,
+    level: 'beg',
+    date: Date.now(),
+    coach: 'Nich',
+    expl: 'LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem,LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem',
+  },
+];
 @Component({
   selector: 'app-admin-profile',
   templateUrl: './admin-profile.component.html',
   styleUrls: ['./admin-profile.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class AdminProfileComponent implements OnInit {
+  displayedColumns: string[] = ['Position', 'Level', 'Date', 'Coach'];
+
+  dataSource = ELEMENT_DATA;
+
+  expandedElement: PeriodicElement | undefined;
+
   ngOnInit(): void {}
 }
--- exadel-trainee-app/src/app/models/admin-profile/admin-profile.module.ts
+++ exadel-trainee-app/src/app/models/admin-profile/admin-profile.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AdminProfileComponent } from './admin-profile.component';
+import { CommonModule, DatePipe } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+import { ScrollDispatcher, ScrollingModule } from '@angular/cdk/scrolling';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { TableVirtualScrollModule } from 'ng-cdk-table-virtual-scroll';
 import { AdminProfileRouting } from './admin-profile-routing';
+import { AdminProfileComponent } from './admin-profile.component';
 
 @NgModule({
   declarations: [AdminProfileComponent],
-  imports: [CommonModule, AdminProfileRouting],
+  imports: [
+    CommonModule,
+    AdminProfileRouting,
+    MatTableModule,
+    MatExpansionModule,
+    TableVirtualScrollModule,
+    ScrollingModule,
+  ],
+  providers: [DatePipe, ScrollDispatcher],
 })
 export class AdminProfileModule {}
