d30095a8f59f4e418b51d6ad6988ccf1880d129c
Dima Halaiskiy <dgalaiskiy@icloud.com>
2021-07-17 00:30:03 +0300

    mmm

--- exadel-trainee-app/src/app/models/speaking-test/speaking-test.module.ts
+++ exadel-trainee-app/src/app/models/speaking-test/speaking-test.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { SpeakingTestComponent } from './speaking-test.component';
 import { SpeakingTestRoutingModule } from './speaking-test-routing.module';
 
 @NgModule({
-  imports: [SpeakingTestRoutingModule, CommonModule, FormsModule, MatButtonModule],
+  imports: [SpeakingTestRoutingModule, CommonModule, FormsModule, MatButtonModule, MatCardModule],
   declarations: [SpeakingTestComponent],
 })
 export class SpeakingTestModule {}
