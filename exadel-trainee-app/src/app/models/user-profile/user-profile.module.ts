cd4ec765c1c96e0be872ba56d5d94238d7d256a5
tanyushanel <tanyushanel@users.noreply.github.com>
2021-07-16 16:31:27 +0200

    refactor: moved notification to sidebar

--- exadel-trainee-app/src/app/models/user-profile/user-profile.module.ts
+++ exadel-trainee-app/src/app/models/user-profile/user-profile.module.ts
@@ -4,10 +4,9 @@ import { AngularMaterialCommonModule } from '../angular-material-common.module';
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { UserProfileComponent } from './user-profile.component';
 import { UserResultsTableComponent } from './user-results-table/user-results-table.component';
-import { NotificationComponent } from '../../components/sidebar/notification/notification.component';
 
 @NgModule({
-  declarations: [UserProfileComponent, UserResultsTableComponent, NotificationComponent],
+  declarations: [UserProfileComponent, UserResultsTableComponent],
   imports: [CommonModule, UserProfileRoutingModule, AngularMaterialCommonModule],
 })
 export class UserProfileModule {}
