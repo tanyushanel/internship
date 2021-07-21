506bef1d3db3da56b8c5443d837a17d939d9d90e
shuliak leonid <zeleny777@gmail.com>
2021-07-21 18:16:09 +0300

    change file directory on level up

--- exadel-trainee-app/src/app/components/mock-component/editor/editor.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AngularMaterialCommonModule } from '../../../models/angular-material-common.module';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, AngularMaterialCommonModule],
-})
-export class CoachProfileEditorModule {}
--- exadel-trainee-app/src/app/interfaces/error.ts
+++
@@ -1,4 +0,0 @@
-export interface ErrorModel {
-  message: string;
-  time: number;
-}
