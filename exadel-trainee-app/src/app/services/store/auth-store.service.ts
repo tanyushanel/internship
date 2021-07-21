7cce26c6715fa813a97e54167d274ea22150d7bd
shuliak leonid <zeleny777@gmail.com>
2021-07-21 15:15:25 +0300

    fix delete file for deploy

--- exadel-trainee-app/src/app/services/store/auth-store.service.ts
+++ exadel-trainee-app/src/app/services/store/auth-store.service.ts
@@ -7,15 +7,14 @@ import { UserResponseType } from '../../../interfaces/user.interfaces';
 import { ErrorStoreService } from './error-store.service';
 import { SignIn } from '../../interfaces/user';
 import { Route } from '../../../constants/route-constant';
-import { DataStoreService } from './data-store.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthStoreService {
-  private readonly initialState = this.dataUser.getUserDataFromStorage();
+  private readonly initialState = null;
 
-  private readonly userSubject$ = new BehaviorSubject<UserResponseType>(this.initialState);
+  private readonly userSubject$ = new BehaviorSubject<UserResponseType | null>(this.initialState);
 
   readonly activeUser$ = this.userSubject$.asObservable();
 
@@ -25,26 +24,20 @@ export class AuthStoreService {
     private readonly authHttpService: AuthHttpService,
     private readonly errorStoreService: ErrorStoreService,
     private readonly router: Router,
-    private dataUser: DataStoreService,
-  ) {
-    console.log(this.dataUser, 'dataUser');
-  }
+  ) {}
 
   private get user(): UserResponseType | null {
     return this.userSubject$.getValue();
   }
 
   private set user(user: UserResponseType | null) {
-    if (user) {
-      this.userSubject$.next(user);
-    }
+    this.userSubject$.next(user);
   }
 
   signIn(signInModel: SignIn): void {
     this.authHttpService.signIn(signInModel).subscribe({
       next: (user) => {
         this.user = { ...user };
-        this.setToken(user);
         this.router.navigate([Route.home]);
       },
       error: (e: Error) => {
@@ -58,21 +51,6 @@ export class AuthStoreService {
 
   signOut(): void {
     this.user = this.initialState;
-    this.setToken(null);
-    localStorage.clear();
     this.router.navigate(['/']);
   }
-
-  private setToken(response: UserResponseType | null) {
-    if (response) {
-      localStorage.setItem('token', response.token);
-      localStorage.setItem('user', JSON.stringify(response));
-    } else {
-      localStorage.clear();
-    }
-  }
-  //
-  // isAuthenticated() {
-  //   return !!this.user;
-  // }
 }
