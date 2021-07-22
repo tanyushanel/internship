74dad395bba4c8b0c8449e99afc6dbb90c1f35b5
tanyushanel <tanyushanel@users.noreply.github.com>
2021-07-22 15:48:08 +0200

    feat: timer-component created

---
+++ src/app/components/timer/timer.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss'],
+})
+export class TimerComponent implements OnInit {
+  ngOnInit(): void {}
+}
