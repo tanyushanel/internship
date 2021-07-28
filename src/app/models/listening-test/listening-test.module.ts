6bd0529c9ab4b2fe06fff343c05bf8c937ecc7a8
Dima Halaiskiy <dgalaiskiy@icloud.com>
2021-07-28 17:02:05 +0300

    add audioplayer whit mockup audio on english

--- src/app/models/listening-test/listening-test.module.ts
+++ src/app/models/listening-test/listening-test.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListeningTestComponent } from './listening-test.component';
 import { StepperModule } from '../../components/stepper/stepper.module';
+import { AngularMaterialCommonModule } from '../angular-material-common.module';
 
 @NgModule({
   declarations: [ListeningTestComponent],
-  imports: [CommonModule, StepperModule],
+  imports: [CommonModule, StepperModule, AngularMaterialCommonModule],
   exports: [ListeningTestComponent],
 })
 export class ListeningTestModule {}
