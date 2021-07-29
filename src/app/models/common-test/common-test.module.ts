2ba674dfe294e8ffae06cb7081fe8ebe6a6e53bf
tanyushanel <tanyushanel@users.noreply.github.com>
2021-07-29 12:40:45 +0200

    refactor: fix styles for stepper

--- src/app/models/common-test/common-test.module.ts
+++ src/app/models/common-test/common-test.module.ts
@@ -4,10 +4,10 @@ import { CommonTestComponent } from './common-test.component';
 import { AngularMaterialCommonModule } from '../angular-material-common.module';
 import { WritingTestModule } from '../writing-test/writing-test.module';
 import { SpeakingTestModule } from '../speaking-test/speaking-test.module';
-import { ListeningTestModule } from '../listening-test/listening-test.module';
 import { GrammarTestModule } from '../grammar-test/grammar-test.module';
-import { CommonTestRoutingModule } from './common-test-routing.module';
 import { ProceedButtonsComponent } from './proceed-buttons/proceed-buttons.component';
+import { ListeningTestModule } from '../listening-test/listening-test.module';
+import { CommonTestRoutingModule } from './common-test-routing.module';
 
 @NgModule({
   declarations: [CommonTestComponent, ProceedButtonsComponent],
