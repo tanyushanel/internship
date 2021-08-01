9fd9791346b1dcd8810c5b0c28e59793bc9b4e01
Dima Halaiskiy <dgalaiskiy@icloud.com>
2021-08-01 23:43:00 +0300

    add after-view-checked component for hidden the repeat button

--- src/app/models/listening-test/listening-test.component.ts
+++ src/app/models/listening-test/listening-test.component.ts
@@ -1,6 +1,14 @@
 import { hostViewClassName } from '@angular/compiler';
-import { Component, ElementRef, Input, AfterViewInit, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  AfterViewInit,
+  OnInit,
+  AfterViewChecked,
+} from '@angular/core';
 import { Track } from 'ngx-audio-player';
+import { repeat } from 'rxjs/operators';
 import { MOCK_QUESTION_LIST_AUDITION, Question } from '../../../constants/mock-grammar-test';
 
 @Component({
@@ -8,7 +16,7 @@ import { MOCK_QUESTION_LIST_AUDITION, Question } from '../../../constants/mock-g
   templateUrl: './listening-test.component.html',
   styleUrls: ['./listening-test.component.scss'],
 })
-export class ListeningTestComponent implements OnInit, AfterViewInit {
+export class ListeningTestComponent implements OnInit, AfterViewInit, AfterViewChecked {
   constructor(private elementRef: ElementRef) {}
 
   questions: Question[] = [];
@@ -36,7 +44,7 @@ export class ListeningTestComponent implements OnInit, AfterViewInit {
   msaapPlaylist: Track[] = [
     {
       title: 'Audio One Title',
-      link: 'https://a1.dlshare.net/sdl/22/b4/d9/86511152_83268387.mp3?name=uroki-angliyskogo--urok-01.mp3&id=3138901',
+      link: 'https://media1.vocaroo.com/mp3/11lNyvchRTGj',
       artist: 'Artist',
       duration: 5,
     },
@@ -52,17 +60,24 @@ export class ListeningTestComponent implements OnInit, AfterViewInit {
       .addEventListener('click', this.onClick.bind(this));
   }
 
-  onClick() {
+  ngAfterViewChecked() {
     const repeatButton = this.elementRef.nativeElement.querySelector(
-      '#mat-tab-content-0-1 > div > app-listening > ngx-audio-player > mat-card > button.mat-focus-indicator.ngx-p-1.volume.justify-content-center.mat-button.mat-button-base.ng-star-inserted',
+      '#mat-tab-content-0-1 > div > app-listening > ngx-audio-player > mat-card > button.mat-focus-indicator.ngx-p-1.volume.justify-content-center.mat-button.mat-button-base.ng-star-inserted:not(.play-repeat-disable)',
     );
+    if (repeatButton) {
+      repeatButton.classList.add('play-repeat-disable');
+      setTimeout(() => {
+        repeatButton.click();
+      });
+    }
+  }
+
+  onClick() {
     const playButton = this.elementRef.nativeElement.querySelector('.play-pause');
     playButton.classList.add('play-pause-disable');
-    repeatButton.classList.add('play-repeat-disable');
     this.counter += 1;
     if (this.counter === 1) {
       playButton.classList.add('play-pause-disable');
-      repeatButton.click();
     }
   }
 
