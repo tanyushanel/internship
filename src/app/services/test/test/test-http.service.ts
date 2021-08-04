18a6b2d267eb7a19216db32dfc0e4972e8a8889d
Andrei <casterweb@tut.by>
2021-08-04 22:25:11 +0300

    bug fix

--- src/app/services/test/test/test-http.service.ts
+++ src/app/services/test/test/test-http.service.ts
@@ -6,7 +6,7 @@ import { Test } from '../../../interfaces/test';
 import { BASE_API_URL } from '../../../../constants/route-constant';
 
 interface GetTestsResult {
-  tests: Test[];
+  results: Test[];
 }
 
 @Injectable({
@@ -20,7 +20,7 @@ export class TestHttpService {
       this.http
         // .get<GetTestsResult>(`${BASE_API_URL}/api/Test?userId=${userId}`)
         .get<GetTestsResult>(`${BASE_API_URL}/api/Test?userId=9d71d2fb-d63e-4fd6-8acf-08d955831c7b`)
-        .pipe(map((res) => res.tests.filter((r) => r.testPassingDate !== null)))
+        .pipe(map((res) => res.results.filter((r) => r.testPassingDate !== null)))
     );
   }
 }
