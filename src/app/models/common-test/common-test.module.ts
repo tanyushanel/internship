1abc1a4c8a43c18ce127aa775ea61df54a9d522c
ExxZzyy <66119764+ExxZzyy@users.noreply.github.com>
2021-08-19 15:19:39 +0300

    Move report mistake dialog component

--- src/app/models/common-test/common-test.module.ts
+++ src/app/models/common-test/common-test.module.ts
@@ -8,9 +8,10 @@ import { GrammarTestModule } from '../grammar-test/grammar-test.module';
 import { ProceedButtonsComponent } from './proceed-buttons/proceed-buttons.component';
 import { ListeningTestModule } from '../listening-test/listening-test.module';
 import { CommonTestRoutingModule } from './common-test-routing.module';
+import { ReportMistakeDialogComponent } from '../../components/report-mistake-dialog/report-mistake-dialog.component';
 
 @NgModule({
-  declarations: [CommonTestComponent, ProceedButtonsComponent],
+  declarations: [CommonTestComponent, ProceedButtonsComponent, ReportMistakeDialogComponent],
   imports: [
     CommonModule,
     AngularMaterialCommonModule,
