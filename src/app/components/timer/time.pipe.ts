add19e8b18012bcac35bdd958eacb010089b4eee
tanyushanel <tanyushanel@users.noreply.github.com>
2021-08-23 17:20:43 +0200

    fix: fix

--- src/app/components/timer/time.pipe.ts
+++ src/app/components/timer/time.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimePipe implements PipeTransform {
   transform(value: number): string {
-    const minutes: number = Math.floor(value / 60);
+    const minutes: number = Math.floor((value % 3600) / 60);
     const seconds: number = Math.floor(value - minutes * 60);
     return `${`00${minutes}`.slice(-2)}:${`00${seconds}`.slice(-2)}`;
   }
