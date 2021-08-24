41f9fa8d22a1e3fe48bae3ad9a824f9825d7c76e
tanyushanel <tanyushanel@users.noreply.github.com>
2021-08-24 22:27:14 +0200

    fix: fix trigger in user-results-table

--- src/app/components/user-results-table/user-results-table.component.ts
+++ src/app/components/user-results-table/user-results-table.component.ts
@@ -14,9 +14,9 @@ import { TestResult, TestResultWithTotal } from '../../interfaces/test';
   styleUrls: ['./user-results-table.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('collapsed, void', style({ height: '0px', minHeight: '0' })),
       state('expanded', style({ height: '*' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
 })
