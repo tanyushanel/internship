29900631fe4b7aad5002bebbbb470890ebcfb922
unknown <1810198nm@gmail.com>
2021-08-24 17:09:55 -0400

    finished with pagination and sorf, working on filtering

--- src/app/models/admin-profile/admin-table/table.component.ts
+++ src/app/models/admin-profile/admin-table/table.component.ts
@@ -19,46 +19,30 @@ import {
   TestData,
   UpdateCoachesData,
 } from '../../../interfaces/admin-profile-intarfaces';
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+import { CoachTest } from '../../../interfaces/coach-edit';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
-export class TableComponent implements AfterViewInit {
+export class TableComponent {
+  @ViewChild(MatTable) table!: MatTable<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  @Input() tableData!: TestData[];
+
+  @Input() coachData!: ServiceCoachData | null;
+
+  @Input() displayedColumns!: string[];
+
+  @Input() selectedTab!: AdminTestTabs;
+
+  dataSource: MatTableDataSource<TestData>;
+
   Level: string[] = [
     'Elementary',
     'PreIntermediate',
@@ -73,45 +57,13 @@ export class TableComponent implements AfterViewInit {
 
   temp: string | null | undefined;
 
-  @Input() tableData!: TestData[];
-
-  @Input() coachData!: ServiceCoachData | null;
-
-  @Input() displayedColumns!: string[];
-
-  @Input() selectedTab!: AdminTestTabs;
-
-  constructor(
-    private dialog: MatDialog,
-    private service: AdminTableStoreService,
-    private changeDetectorRefs: ChangeDetectorRef,
-  ) {
-    console.log(this.tableData);
-  }
-
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-
-  displayedColumns1: string[] = ['position', 'name', 'weight', 'symbol'];
-
-  @ViewChild(MatTable) table!: MatTable<any>;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  @ViewChild(MatSort) sort!: MatSort;
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-
-    setTimeout(() => {
-      const sortState: Sort = { active: 'date', direction: 'desc' };
-      this.sort.active = sortState.active;
-      this.sort.direction = sortState.direction;
-      this.sort.sortChange.emit(sortState);
-    }, 10);
+  constructor(private dialog: MatDialog, private service: AdminTableStoreService) {
+    this.dataSource = new MatTableDataSource(this.tableData);
   }
 
   ngOnChanges() {
+    this.dataSource.data = this.tableData;
+    this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
@@ -132,6 +84,7 @@ export class TableComponent implements AfterViewInit {
           coachId: result?.coach?.userId,
         };
         this.service.updateTestData(this.coachUpdate, result.id);
+        this.dataSource.data = this.tableData;
       }
     });
   }
