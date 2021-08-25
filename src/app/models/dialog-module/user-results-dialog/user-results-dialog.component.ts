9d761a5600e95c6690cbaaceecb353460949ca9b
shuliak leonid <zeleny777@gmail.com>
2021-08-25 09:22:38 +0300

    add logo in user group,fix display logo icon

--- src/app/models/dialog-module/user-results-dialog/user-results-dialog.component.ts
+++ src/app/models/dialog-module/user-results-dialog/user-results-dialog.component.ts
@@ -1,11 +1,12 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
-import { Level } from 'src/app/constants/data-constants';
 import { TestStoreService } from 'src/app/services/store/test-store.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { user } from '../../../constants/mock-user-data';
 import { TestResult } from '../../../interfaces/test';
 import { GetHrUser } from '../../../interfaces/user.interfaces';
+import { CoachAudioDataStoreService } from '../../../services/store/coach-audio-data-store.service';
 
 @Component({
   selector: 'app-user-results-dialog',
@@ -21,6 +22,8 @@ export class UserResultsDialogComponent implements OnInit {
 
   results: TestResult[] = [];
 
+  imgFilePath: SafeUrl | undefined;
+
   get testsCount() {
     return this.results.length;
   }
@@ -29,10 +32,18 @@ export class UserResultsDialogComponent implements OnInit {
     private testStoreService: TestStoreService,
     public dialogRef: MatDialogRef<UserResultsDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: GetHrUser,
+    private readonly audioData: CoachAudioDataStoreService,
+    private sanitizer: DomSanitizer,
   ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.user = { ...user, ...this.data };
+    const blob = await this.audioData.fetchUrlAudio(this.user.avatar);
+    const reader = new FileReader();
+    reader.readAsDataURL(<Blob>blob);
+    reader.onload = () => {
+      this.imgFilePath = this.sanitizer.bypassSecurityTrustUrl(reader.result as string);
+    };
   }
 
   onAssignBtnClick(): void {
